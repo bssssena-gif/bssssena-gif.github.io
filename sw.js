7d48dfaad186023ef40d65bfcab4ea7be8a4540a
bssssena-gif <bssssena@gmail.com>
2026-06-14 10:56:14 +0530

    Update CACHE_NAME to docnear-v13

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    File: sw.js (same folder as DocNear.html)
    ═══════════════════════════════════════════════ */
 
-const CACHE_NAME = "docnear-v12";
+const CACHE_NAME = "docnear-v13";
 const STATIC_ASSETS = [
   "./index.html",
   "./DocNear.css",
